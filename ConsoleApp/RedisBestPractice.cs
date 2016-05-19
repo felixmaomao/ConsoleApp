68474fb56d29b772957ce8b151f583e91ed98060
swfightforyl <swfightforyl@163.com>
2016-05-20 07:31:19 +0800

    redis best practice

---
+++ ConsoleApp/RedisBestPractice.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.Redis;
+namespace ConsoleApp
+{
+    public class RedisBestPractice
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+
+    #region Blog Models
+  
+    public class User:IHasBlogRepository
+    {
+        public User()
+        {
+            this.BlogIds = new List<long>();           
+        }
+        public IBlogRepository Repo
+        {
+            get;
+            set;
+        }
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public List<long> BlogIds { get; set; }
+        public List<Blog> GetBlogs()
+        {
+            return Repo.GetAllBlogs();
+        }
+        public Blog CreateNewBlog(Blog blog)
+        {
+            this.Repo.StoreBlogs(this,blog);
+            return blog;
+        }
+        public BlogPost WriteBlogPost(Blog blog,BlogPost post)
+        {
+            this.Repo.StoreNewBlogPosts(blog,post);
+            return post;
+        }
+
+    }
+    public class Blog:IHasBlogRepository
+    {
+        public long ID { get; set; }
+        public string BlogName { get; set; }
+        public long UserID { get; set; }
+        public string UserName { get; set;}
+        public List<string> Tags { get; set; }
+        public List<long> BlogPostIds { get; set; }
+        public IBlogRepository Repo
+        {
+            get;
+            set;
+        }
+        public List<BlogPost> GetBlogPosts()
+        {
+            return this.Repo.GetBlogPosts(this.BlogPostIds);
+        }
+        public void StoreNewBlogPosts(params BlogPost[] Posts)
+        {
+            this.Repo.StoreNewBlogPosts(this,Posts);
+        }
+    }
+    public class BlogPost
+    {
+        public BlogPost()
+        {
+            this.categories = new List<string>();
+            this.Tags = new List<string>();
+            this.Comments = new List<BlogPostComment>();
+        }
+        public long Id { get; set; }
+        public long BlogId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public List<string> categories { get; set; }
+        public List<string> Tags { get; set; }
+        public List<BlogPostComment> Comments { get; set; }
+
+    }
+    public class BlogPostComment
+    {
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+    #endregion
+
+    #region Blog Repositories
+    public interface IBlogRepository
+    {
+        void StoreUsers(params User[] Users);
+        List<User> GetAllUsers();
+
+        void StoreBlogs(User user,params Blog[] Blogs);
+        List<Blog> GetBlogs(IEnumerable<long> Blogids);
+        List<Blog> GetAllBlogs();
+
+        List<BlogPost> GetBlogPosts(IEnumerable<long> blogPostsIds);
+        void StoreNewBlogPosts(Blog blog,params BlogPost[] blogPosts);
+
+        List<BlogPost> GetRecentBlogPosts();
+        List<BlogPostComment> GetRecentBlogPostComments();
+        IDictionary<string, double> GetTopTags(int take);
+        HashSet<string> GetAllCategories();
+
+        void StoreBlogPost(BlogPost blogPost);
+        BlogPost GetBlogPost(params long[] postid);
+        List<BlogPost> GetBlogPostByCategory(string CategoryName);
+
+    }
+    public interface IHasBlogRepository
+    {
+        IBlogRepository Repo { get; set; }
+    }
+    public class BlogRepository:IBlogRepository
+    {
+        private readonly IRedisClient redis;
+        public BlogRepository(IRedisClient client)
+        {
+            this.redis = client;
+        }
+        public void StoreUsers(params User[] Users)
+        {
+            var redisUsers=redis.As<User>();
+            foreach(var item in Users.Where(x=>x.ID==default(int)))
+            {
+                item.ID = redisUsers.GetNextSequence();
+            }
+            redisUsers.StoreAll(Users);           
+        }
+
+        public List<User> GetAllUsers()
+        {
+            var redisUsers=redis.As<User>();
+            return redisUsers.GetAll().ToList();
+        }
+
+        public void StoreBlogs(User user,params Blog[] Blogs)
+        {
+            var redisBlogs=redis.As<Blog>();
+            foreach(var blog in Blogs)
+            {
+                if(blog.ID==default(long))
+                {
+                    blog.ID = redisBlogs.GetNextSequence();
+                }
+                blog.UserID = user.ID;
+                blog.UserName = user.Name;
+                user.BlogIds.Add(blog.ID);
+            }
+            using(var trans=redis.CreateTransaction())
+            {
+                trans.QueueCommand(x=>x.Store(user));
+                trans.QueueCommand(x=>x.StoreAll(Blogs));
+                trans.Commit();
+            }
+        }
+
+        public List<Blog> GetBlogs(IEnumerable<long> Blogids)
+        {
+            var redisBlogs=redis.As<Blog>();
+            return redisBlogs.GetByIds(Blogids).ToList();
+        }
+
+        public List<Blog> GetAllBlogs()
+        {
+            var redisBlogs=redis.As<Blog>();
+            return redisBlogs.GetAll().ToList();
+        }
+
+        public List<BlogPost> GetBlogPosts(IEnumerable<long> blogPostsIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StoreNewBlogPosts(Blog blog, params BlogPost[] blogPosts)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BlogPost> GetRecentBlogPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BlogPostComment> GetRecentBlogPostComments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, double> GetTopTags(int take)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HashSet<string> GetAllCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StoreBlogPost(BlogPost blogPost)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BlogPost GetBlogPost(params long[] postid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BlogPost> GetBlogPostByCategory(string CategoryName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    #endregion
+}
--- ConsoleApp/redis测试.cs
+++ ConsoleApp/redis测试.cs
@@ -1,234 +1,234 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ServiceStack.Redis;
-using ServiceStack.Redis.Generic;
-using ServiceStack;
-using ServiceStack.Text;
-namespace ConsoleApp
-{
-    public class redis测试
-    {
-        static readonly RedisClient redis = new RedisClient("127.0.0.1",6379);
-        public static void Main(string[] args)
-        {
-            redis.FlushAll();
-            InsertTestData();
-            //show_a_list_of_blogs();
-            //show_rencent_posts_and_comments();
-            //show_a_tag_cloud();
-            //show_post_and_all_comments();
-            //add_comment_to_existing_post();
-            //add_string_test();
-            delete_a_post(2);
-            Console.ReadKey();
-        }
-        public static void InsertTestData()
-        {
-            var redisUsers = redis.As<User>();
-            var redisBlogs = redis.As<Blog>();
-            var redisBlogPosts = redis.As<BlogPost>();
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using ServiceStack.Redis;
+//using ServiceStack.Redis.Generic;
+//using ServiceStack;
+//using ServiceStack.Text;
+//namespace ConsoleApp
+//{
+//    public class redis测试
+//    {
+//        static readonly RedisClient redis = new RedisClient("127.0.0.1",6379);
+//        public static void Main(string[] args)
+//        {
+//            redis.FlushAll();
+//            InsertTestData();
+//            //show_a_list_of_blogs();
+//            //show_rencent_posts_and_comments();
+//            //show_a_tag_cloud();
+//            //show_post_and_all_comments();
+//            //add_comment_to_existing_post();
+//            //add_string_test();
+//            delete_a_post(2);
+//            Console.ReadKey();
+//        }
+//        public static void InsertTestData()
+//        {
+//            var redisUsers = redis.As<User>();
+//            var redisBlogs = redis.As<Blog>();
+//            var redisBlogPosts = redis.As<BlogPost>();
 
-            var jason = new User {ID=redisUsers.GetNextSequence(),UserName="jason_shenwei" };
-            var jane = new User {ID=redisUsers.GetNextSequence(),UserName="jane_zhangxiaomao" };
+//            var jason = new User {ID=redisUsers.GetNextSequence(),UserName="jason_shenwei" };
+//            var jane = new User {ID=redisUsers.GetNextSequence(),UserName="jane_zhangxiaomao" };
 
-            var jasonBlog = new Blog {
-                ID = redisBlogs.GetNextSequence(),
-                UserID = jason.ID,
-                UserName = jason.UserName,
-                Tags = new List<string> {"Architecture",".Net","DataBases"}
-            };
+//            var jasonBlog = new Blog {
+//                ID = redisBlogs.GetNextSequence(),
+//                UserID = jason.ID,
+//                UserName = jason.UserName,
+//                Tags = new List<string> {"Architecture",".Net","DataBases"}
+//            };
 
-            var janeBlog = new Blog {
-                ID=redisBlogs.GetNextSequence(),
-                UserID=jane.ID,
-                UserName=jane.UserName,
-                Tags=new List<string> {"Architecture","Winform","DataBases"}
-            };
+//            var janeBlog = new Blog {
+//                ID=redisBlogs.GetNextSequence(),
+//                UserID=jane.ID,
+//                UserName=jane.UserName,
+//                Tags=new List<string> {"Architecture","Winform","DataBases"}
+//            };
 
-            var blogPosts = new List<BlogPost> {
-                new BlogPost {
-                    ID=redisBlogPosts.GetNextSequence(),
-                    BlogId=jasonBlog.ID,
-                    Title="nosql",
-                    Tags=new List<string> {"nosql","redis" },
-                    Comments=new List<BlogPostComment> {
-                        new BlogPostComment { Content="first comment!",CreatedDate=DateTime.Now}
-                    }                  
-                },
-                  new BlogPost {
-                    ID=redisBlogPosts.GetNextSequence(),
-                    BlogId=jasonBlog.ID,
-                    Title="redis learning",
-                    Tags=new List<string> {"database","redis" },
-                    Comments=new List<BlogPostComment> {
-                        new BlogPostComment { Content="second comment!",CreatedDate=DateTime.Now}
-                    }
-                },
-                    new BlogPost {
-                    ID=redisBlogPosts.GetNextSequence(),
-                    BlogId=janeBlog.ID,
-                    Title="winform",
-                    Tags=new List<string> {"winform" },
-                    Comments=new List<BlogPostComment> {
-                        new BlogPostComment { Content="first comment!",CreatedDate=DateTime.Now}
-                    }
-                },
-                      new BlogPost {
-                    ID=redisBlogPosts.GetNextSequence(),
-                    BlogId=janeBlog.ID,
-                    Title="wpf",
-                    Tags=new List<string> {"wpf","windows" },
-                    Comments=new List<BlogPostComment> {
-                        new BlogPostComment { Content="second comment!",CreatedDate=DateTime.Now}
-                    }
-                }
+//            var blogPosts = new List<BlogPost> {
+//                new BlogPost {
+//                    ID=redisBlogPosts.GetNextSequence(),
+//                    BlogId=jasonBlog.ID,
+//                    Title="nosql",
+//                    Tags=new List<string> {"nosql","redis" },
+//                    Comments=new List<BlogPostComment> {
+//                        new BlogPostComment { Content="first comment!",CreatedDate=DateTime.Now}
+//                    }                  
+//                },
+//                  new BlogPost {
+//                    ID=redisBlogPosts.GetNextSequence(),
+//                    BlogId=jasonBlog.ID,
+//                    Title="redis learning",
+//                    Tags=new List<string> {"database","redis" },
+//                    Comments=new List<BlogPostComment> {
+//                        new BlogPostComment { Content="second comment!",CreatedDate=DateTime.Now}
+//                    }
+//                },
+//                    new BlogPost {
+//                    ID=redisBlogPosts.GetNextSequence(),
+//                    BlogId=janeBlog.ID,
+//                    Title="winform",
+//                    Tags=new List<string> {"winform" },
+//                    Comments=new List<BlogPostComment> {
+//                        new BlogPostComment { Content="first comment!",CreatedDate=DateTime.Now}
+//                    }
+//                },
+//                      new BlogPost {
+//                    ID=redisBlogPosts.GetNextSequence(),
+//                    BlogId=janeBlog.ID,
+//                    Title="wpf",
+//                    Tags=new List<string> {"wpf","windows" },
+//                    Comments=new List<BlogPostComment> {
+//                        new BlogPostComment { Content="second comment!",CreatedDate=DateTime.Now}
+//                    }
+//                }
 
-            };
+//            };
 
-            //add references
-            jason.BlogIds.Add(jasonBlog.ID);
-            jasonBlog.BlogPostIds.AddRange(blogPosts.Where(x=>x.BlogId==jasonBlog.ID).Map(x=>x.ID));
+//            //add references
+//            jason.BlogIds.Add(jasonBlog.ID);
+//            jasonBlog.BlogPostIds.AddRange(blogPosts.Where(x=>x.BlogId==jasonBlog.ID).Map(x=>x.ID));
 
-            jane.BlogIds.Add(janeBlog.ID);
-            janeBlog.BlogPostIds.AddRange(blogPosts.Where(x=>x.BlogId==janeBlog.ID).Map(x=>x.ID));
+//            jane.BlogIds.Add(janeBlog.ID);
+//            janeBlog.BlogPostIds.AddRange(blogPosts.Where(x=>x.BlogId==janeBlog.ID).Map(x=>x.ID));
 
-            redisUsers.Store(jane);
-            redisUsers.Store(jason);
+//            redisUsers.Store(jane);
+//            redisUsers.Store(jason);
 
-            redisBlogs.StoreAll(new[] {janeBlog,jasonBlog });
-            redisBlogPosts.StoreAll(blogPosts);
-
-
+//            redisBlogs.StoreAll(new[] {janeBlog,jasonBlog });
+//            redisBlogPosts.StoreAll(blogPosts);
 
 
-        }
-
-        public static void show_a_list_of_blogs()
-        {
-            var redisBlogs = redis.As<Blog>();
-            var blogs = redisBlogs.GetAll();
-            blogs.PrintDump();
-        }
-
-        public static void show_rencent_posts_and_comments()
-        {
-            var redisPosts = redis.As<BlogPost>();
-            var redisComments = redis.As<BlogPostComment>();
-            var incomingposts = redisPosts.GetAll();
-            var recentPosts = redisPosts.Lists["urn:BlogPosts:Recent"];
-            var recentComments = redisComments.Lists["urn:BlogComments:Recent"];
-            foreach (var post in incomingposts)
-            {
-                recentPosts.Prepend(post);
-                post.Comments.ForEach(recentComments.Prepend);
-            }
-            recentPosts.Trim(0,2);
-            recentComments.Trim(0,2);
-            recentPosts.GetAll().PrintDump();
-            recentComments.GetAll().PrintDump();
 
 
-        }
+//        }
 
-        public static void show_a_tag_cloud()
-        {
-            var redisBlogs = redis.As<Blog>();
-            var blogs = redisBlogs.GetAll();
-            foreach (var blog in blogs)
-            {
-                blog.Tags.ForEach(x=>redis.IncrementItemInSortedSet("urn:TagCloud",x,1));
-            }
-            //showtop 5 tags
-            var TagClouds = redis.GetAllItemsFromSortedSetDesc("urn:TagCloud");
-            TagClouds.PrintDump();
-        }
+//        public static void show_a_list_of_blogs()
+//        {
+//            var redisBlogs = redis.As<Blog>();
+//            var blogs = redisBlogs.GetAll();
+//            blogs.PrintDump();
+//        }
 
-        public static void show_post_and_all_comments()
-        {
-            var postid = 1;
-            var redisblogposts = redis.As<BlogPost>();
-            var selectedBlogPost = redisblogposts.GetById(postid);
-            selectedBlogPost.PrintDump();
+//        public static void show_rencent_posts_and_comments()
+//        {
+//            var redisPosts = redis.As<BlogPost>();
+//            var redisComments = redis.As<BlogPostComment>();
+//            var incomingposts = redisPosts.GetAll();
+//            var recentPosts = redisPosts.Lists["urn:BlogPosts:Recent"];
+//            var recentComments = redisComments.Lists["urn:BlogComments:Recent"];
+//            foreach (var post in incomingposts)
+//            {
+//                recentPosts.Prepend(post);
+//                post.Comments.ForEach(recentComments.Prepend);
+//            }
+//            recentPosts.Trim(0,2);
+//            recentComments.Trim(0,2);
+//            recentPosts.GetAll().PrintDump();
+//            recentComments.GetAll().PrintDump();
 
-        }
 
-        public static void add_comment_to_existing_post()
-        {
-            int postid = 1;
-            var redisblogposts = redis.As<BlogPost>();
-            var blogpost = redisblogposts.GetById(postid);
-            blogpost.Comments.Add(new BlogPostComment { Content="nice you are hansome!",CreatedDate=DateTime.Now});
-            redisblogposts.Store(blogpost);
-            var refreshblogpost = redisblogposts.GetById(postid);
-            refreshblogpost.PrintDump();
-        }
-        public static void add_string_test()
-        {
-            redis.Add<string>("urn:str1","hello");
-            redis.Add<string>("urn:str2","hi");
-        }
+//        }
 
-        public static void delete_a_post(int id)
-        {
-            var redisposts = redis.As<BlogPost>();            
-            redisposts.DeleteById(id);
-        }
+//        public static void show_a_tag_cloud()
+//        {
+//            var redisBlogs = redis.As<Blog>();
+//            var blogs = redisBlogs.GetAll();
+//            foreach (var blog in blogs)
+//            {
+//                blog.Tags.ForEach(x=>redis.IncrementItemInSortedSet("urn:TagCloud",x,1));
+//            }
+//            //showtop 5 tags
+//            var TagClouds = redis.GetAllItemsFromSortedSetDesc("urn:TagCloud");
+//            TagClouds.PrintDump();
+//        }
+
+//        public static void show_post_and_all_comments()
+//        {
+//            var postid = 1;
+//            var redisblogposts = redis.As<BlogPost>();
+//            var selectedBlogPost = redisblogposts.GetById(postid);
+//            selectedBlogPost.PrintDump();
+
+//        }
+
+//        public static void add_comment_to_existing_post()
+//        {
+//            int postid = 1;
+//            var redisblogposts = redis.As<BlogPost>();
+//            var blogpost = redisblogposts.GetById(postid);
+//            blogpost.Comments.Add(new BlogPostComment { Content="nice you are hansome!",CreatedDate=DateTime.Now});
+//            redisblogposts.Store(blogpost);
+//            var refreshblogpost = redisblogposts.GetById(postid);
+//            refreshblogpost.PrintDump();
+//        }
+//        public static void add_string_test()
+//        {
+//            redis.Add<string>("urn:str1","hello");
+//            redis.Add<string>("urn:str2","hi");
+//        }
+
+//        public static void delete_a_post(int id)
+//        {
+//            var redisposts = redis.As<BlogPost>();            
+//            redisposts.DeleteById(id);
+//        }
 
         
-    }
+//    }
 
-    public class User
-    {
-        public long ID { get; set; }
-        public string UserName { get; set; }
-        public List<long> BlogIds { get; set; }
-        public User()
-        {
-            BlogIds = new List<long>();
-        }
-    }
-
-
-    public class Blog
-    {
-        public Blog()
-        {
-            this.Tags = new List<string>();
-            this.BlogPostIds = new List<long>();
-        }
-        public long ID { get; set; }
-        public long UserID { get; set; }
-        public string BlogName { get; set; }
-        public string UserName { get; set; }
-        public List<string> Tags { get; set; }
-        public List<long> BlogPostIds { get; set; }
-
-    }
-
-    public class BlogPost
-    {
-        public BlogPost()
-        {
-        }
-        public long ID { get; set; }
-        public long BlogId { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public List<string> Tags { get; set; }
-        public List<BlogPostComment> Comments { get; set; }
-    }
-
-    public class BlogPostComment
-    {
-        public string Content { get; set; }
-        public DateTime CreatedDate { get; set; }
-    }
+//    public class User
+//    {
+//        public long ID { get; set; }
+//        public string UserName { get; set; }
+//        public List<long> BlogIds { get; set; }
+//        public User()
+//        {
+//            BlogIds = new List<long>();
+//        }
+//    }
 
 
+//    public class Blog
+//    {
+//        public Blog()
+//        {
+//            this.Tags = new List<string>();
+//            this.BlogPostIds = new List<long>();
+//        }
+//        public long ID { get; set; }
+//        public long UserID { get; set; }
+//        public string BlogName { get; set; }
+//        public string UserName { get; set; }
+//        public List<string> Tags { get; set; }
+//        public List<long> BlogPostIds { get; set; }
+
+//    }
+
+//    public class BlogPost
+//    {
+//        public BlogPost()
+//        {
+//        }
+//        public long ID { get; set; }
+//        public long BlogId { get; set; }
+//        public string Title { get; set; }
+//        public string Content { get; set; }
+//        public List<string> Tags { get; set; }
+//        public List<BlogPostComment> Comments { get; set; }
+//    }
+
+//    public class BlogPostComment
+//    {
+//        public string Content { get; set; }
+//        public DateTime CreatedDate { get; set; }
+//    }
 
 
 
-}
+
+
+//}
